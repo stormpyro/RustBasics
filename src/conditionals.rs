077dc63277f9821078df0283bfebc4290be7f185
Stormpyro <tatto1795@gmail.com>
2021-05-09 13:56:06 -0500

    Rust Tutorial: Conditionals

--- src/conditionals.rs
+++ src/conditionals.rs
@@ -1,1 +1,18 @@
-pub fn run() {}
+pub fn run() {
+    let age: u8 = 22;
+    let check_id: bool = false;
+    let knows_person_of_age = true;
+
+    // IF/Else
+    if age >= 21 && check_id || knows_person_of_age {
+        println!("Bartender: What would you like to drink?");
+    } else if age < 21 && check_id {
+        println!("Bartender: Sorry, you have to leave");
+    } else {
+        println!("Bartender: I'll need to see your ID")
+    }
+
+    // Shorthand if
+    let is_of_age = if age >= 21 { true } else { false };
+    println!("Is Of Age: {}", is_of_age);
+}
